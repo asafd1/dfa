7257cd811c244e4339afc906abe86dc864800692
Asaf David <adavid@pingidentity.com>
2021-10-10 22:44:49 +0300

    optimize execution time

--- dfa.py
+++ dfa.py
@@ -1,24 +1,21 @@
 import pandas
 
+def filter_rows(df):
+    return (df['Height']>150) & (df['Dye Color'] != 'O')
+
 def tag_rows(df):
     color = df['Dye Color']
     size = df['Size']
-    height = df['Height']
 
     margins = {
         'B' : [497,86],
         'G' : [295,174],
         'R' : [295,121],
-        'Y' : [497,411],
-        'O' : [0,0]
+        'Y' : [497,411]
     }
 
     df['valid'] = False
     df['Group'] = 0
-    if (height < 150):
-        return df
-    if (color == 'O'):
-        return df
 
     if (size >= margins[color][0]-10 and size <= margins[color][0]+10):
         df['Group'] = margins[color][0]
@@ -32,7 +29,9 @@ def tag_rows(df):
 def panda_file():
     df = pandas.read_csv("./dfa.csv")
     df = pandas.DataFrame(df, columns = ['Sample File Name','Dye Color','Size', 'Height', 'Area in Point'])
-    print(df.head())
+    print(df)
+    df = df.loc[filter_rows]
+    print(df)
     df = df.apply(tag_rows, axis=1).query('valid == True')
     df = df.groupby(['Sample File Name','Dye Color','Group'])
     df.sum().reset_index()[['Sample File Name','Dye Color','Group', 'Area in Point']].sort_values(by=['Sample File Name','Dye Color']).to_csv('out.csv', index=False, sep='\t')
